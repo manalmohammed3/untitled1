de7b301b695a4abc3fe4f40cd9ce2c2a9996a8c4
manal mohammed <manal.mohammed.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:15:28 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+const val APPLES=50
+fun main(args: Array<String>) {
+
+
+const val APPLES=65
+    const val DOZENS=12
+    fun main (args: Array<string>){
+
+        val noOfdozens= APPLES/DOZENS
+        val remainApples= APPLES%DOZENS
+
+    }
+
+
+
+
+    println("the num of dozen" + noOfdozens)
+    println(remainApples)
+
+
+}
